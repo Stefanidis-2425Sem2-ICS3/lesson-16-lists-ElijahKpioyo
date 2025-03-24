d70f638d03e456f9b4dee1b94d3ba4442e5b71b7
ElijahKpioyo <kpioyoe@students.dsbn.org>
2025-03-24 12:26:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# E. Kpioyo
+# 100 Number Averager
+# March 24, 2025
+# This program lists 100 numbers and prints the average of them.
 import random
 num = [random.randint(0,100) for i in range(100)]
 
