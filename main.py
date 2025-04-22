58b8d4f8991c0c55fe0b0a9ff87c6d6abf68ee44
ElijahKpioyo <kpioyoe@students.dsbn.org>
2025-04-22 12:20:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,18 @@
 # March 24, 2025
 # This program lists 100 numbers and prints the average of them.
 import random
-num = [random.randint(0,100) for i in range(100)]
+list = []
+
+#num = [random.randint(0,100) for i in range(100)]
+
+
+for i in range(100):
+  num = random.randint(0,100)
+  list.append(num)
 
 print ("The 100 numbers are:")
 print (num)
 
-average = sum(num)/len(num)
+average = sum(list)/len(list)
 
 print ("The average of your numbers is: ", average)
